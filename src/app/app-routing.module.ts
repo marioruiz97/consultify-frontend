ddc976304137de6d06ad0e9f43887102c4c5e5be
marioaruiz97 <marioarb.97@gmail.com>
2024-04-16 11:50:13 -0500

    se agrega ruta formulario se corrije css del listado usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { HomeComponent } from './feature/home/home.component';
 import { InicioSesionComponent } from './feature/autenticacion/components/inicio-sesion/inicio-sesion.component';
 import { RecuperarContrasenaComponent } from './feature/autenticacion/components/recuperar-contrasena/recuperar-contrasena.component';
 import { MiPerfilComponent } from './feature/autenticacion/components/mi-perfil/mi-perfil.component';
+import { ListaUsuariosComponent } from './feature/usuarios/components/lista-usuarios/lista-usuarios.component';
+import { FormularioUsuarioComponent } from './feature/usuarios/components/formulario-usuario/formulario-usuario.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: InicioSesionComponent },
   { path: 'recuperar', component: RecuperarContrasenaComponent },
-  { path: 'cuenta', component: MiPerfilComponent }
+  { path: 'cuenta', component: MiPerfilComponent },
+  { path: 'usuarios', component: ListaUsuariosComponent },
+  { path: 'usuarios/:id', component: FormularioUsuarioComponent }
 ];
 
 @NgModule({
