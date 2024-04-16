68b66f393e89e92b9e6f9a1cf7a7fb1530050f4d
marioaruiz97 <marioarb.97@gmail.com>
2024-04-15 22:25:00 -0500

    minor: refactorizacion

--- src/app/shared/app.constants.ts
+++ src/app/shared/app.constants.ts
@@ -1,11 +1,11 @@
 import { MatDialogConfig } from '@angular/material/dialog';
 
 export class AppConstants {
-  public static PATH_TIPO_CITA = 'tipo-citas';
-  public static PATH_RESPONSABLE = 'responsables';
-  public static PATH_MASCOTA = 'mascotas';
-  public static PATH_VETERINARIO = 'veterinarios';
-  public static PATH_CITA = 'citas';
+  public static readonly PATH_TIPO_CITA = 'tipo-citas';
+  public static readonly PATH_RESPONSABLE = 'responsables';
+  public static readonly PATH_MASCOTA = 'mascotas';
+  public static readonly PATH_VETERINARIO = 'veterinarios';
+  public static readonly PATH_CITA = 'citas';
 }
 
 export const DIALOG_CONFIG: MatDialogConfig = {
